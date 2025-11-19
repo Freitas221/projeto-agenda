c8d570e0d085339a7ec491f28274f27963c8150e
marcosfreitas221 <marcosfreitas7542@gmail.com>
2025-11-19 17:43:14 -0300

    Implementing the nav

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,5 @@
 import './assets/css/style.css' 
 import 'core-js/stable' // pedaço de código que "imita" funcionalidades modernas em navegadores/ambientes antigos.
 import 'regenerator-runtime/runtime' // serve para dar suporte a funções assíncronas (async/await) e generators em ambientes que não entendem isso nativamente.
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
