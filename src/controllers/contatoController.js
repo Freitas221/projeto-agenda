b6a25ee8636c807c34d7c7500ae17e2618168c14
marcosfreitas221 <marcosfreitas7542@gmail.com>
2026-03-04 09:30:55 -0300

    feat: create Contact controller and model

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -1,7 +1,29 @@
+const Contato = require('../models/ContatoModel')
+
 exports.index = (req, res) => {
     res.render('contato')
 }
 
-exports.register = (req, res) => {
-    res.render('contato')
+exports.register = async (req, res) => {
+    try {
+        const contato  = new Contato(req.body)
+        await contato.register()
+
+
+        if(contato.errors.length > 0) {
+        req.flash('errors', contato.errors)
+                req.session.save(() => {
+                return res.redirect(req.get('Referrer' || 'back'))
+                })
+                return   
+            }
+
+            req.flash('success', 'Contato criado com sucesse')
+            req.session.save(() => {
+                return res.redirect(req.get('Referrer' || 'back'))
+            })   
+        }catch(e) {
+            console.log(e)
+            return res.render('404')
+    }
 }
