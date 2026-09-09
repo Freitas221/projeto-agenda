ce84d8e3564a033d1f532dd9b86de6c3a5f21d76
marcosfreitas221 <marcosfreitas7542@gmail.com>
2026-09-09 17:48:57 -0300

    feat(contatoContoller): Starting the implementation of the contact editing method – Part 2

--- src/controllers/contatoController.js
+++ src/controllers/contatoController.js
@@ -43,3 +43,28 @@ exports.editContact = async function(req, res) {
         
     res.render('contato', { contato })
 } 
+
+exports.edit = async(req, res) => {
+    
+    try {
+        if(!req.params.id) return res.render('404')
+        
+        const contato  = new Contato(req.body)
+        await contato.edit(req.params.id)
+
+        if(contato.errors.length > 0) {
+            req.flash('errors', contato.errors)
+            req.session.save(function() {
+                return res.redirect(req.get(('Referrer') || '/contato'))
+            })
+            return
+        }
+
+        req.flash('succes', 'Contato editado com sucesso')
+        req.session.save(()=> {
+            res.redirect(`/contato/${contato.contato._id}`)
+        })
+    }catch(e) {
+        throw new Error(e.getMessage())
+    }
+}
